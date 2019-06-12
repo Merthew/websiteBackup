d9cf2bc9d920e6b0a364d514883aa221b4f4edd8
Timothy-Patterson <timjpatt@gmail.com>
2019-06-12 19:24:48 -0400

    Stylistic Things

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 function changeIFrame(num){
 	
-	var iframe = document.getElementById("VectorWorkFrame");
+	var iframe = document.getElementById("Frame");
 	
 	switch(num){
 	case 0:
-		console.log("Case 0.");
 		iframe.src = "Home/home.html";
 		break;
 	case 1:
-		console.log("Case 1.");
 		iframe.src = "VectorWork/vectorwork.html";
 		break;
 	}
 	
+}
+
+function shadeButton(num) {
+	switch(num){
+	case 0:
+		var shadeButton = document.getElementById("bHome");
+		shadeButton.style.backgroundColor = "#B5B5B5";
+		break;
+	case 1:
+		var shadeButton = document.getElementById("bVectorWork");
+		shadeButton.style.backgroundColor = "#B5B5B5";
+		break;
+	}
+}
+
+function unshadeButton(num) {
+	switch(num){
+	case 0:
+		var shadeButton = document.getElementById("bHome");
+		shadeButton.style.backgroundColor = "#FFF";
+		break;
+	case 1:
+		var shadeButton = document.getElementById("bVectorWork");
+		shadeButton.style.backgroundColor = "#FFF";
+		break;
+	}
 }
