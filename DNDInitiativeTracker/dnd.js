e679274f60e98077af58bcd62584f3757e5c0b8f
Merthew <timjpatt@gmail.com>
2019-09-26 14:28:21 -0400

    Update

--- DNDInitiativeTracker/dnd.js
+++ DNDInitiativeTracker/dnd.js
@@ -1,6 +1,8 @@
 let names = [];
 let val = [];
 
+let pastNames = [];
+
 function addElement(){
 	var name = document.getElementById("inputName").value;
 	var ini = document.getElementById("inputIni").value;
@@ -11,27 +13,13 @@ function addElement(){
 	
 	names.push("Name: " + name + "\t\tInititive: ");
 	val.push(ini);
+	if(!pastNames.includes(name)){pastNames.push(name);}
 	
-	var list = [];
-	for (var j = 0; j < names.length; j++) 
-		list.push({'name': names[j], 'age': val[j]});
-
-	//2) sort:
-	list.sort(function(a, b) {
-		return ((Number(a.age) > Number(b.age)) ? -1 : ((Number(a.age) == Number(b.age)) ? 0 : 1));
-		//Sort could be modified to, for example, sort on the age 
-		// if the name is the same.
-	});
-
-	//3) separate them back out:
-	for (var k = 0; k < list.length; k++) {
-		names[k] = list[k].name;
-		val[k] = list[k].age;
-	}
+	sortArrays();
 	
-	const myNode = document.getElementById("itemList");
-	while (myNode.firstChild) {
-		myNode.removeChild(myNode.firstChild);
+	const itemListNode = document.getElementById("itemList");
+	while (itemListNode.firstChild) {
+		itemListNode.removeChild(itemListNode.firstChild);
 	}
 	
 	for(let i = 0; i < names.length; i ++){
@@ -43,15 +31,89 @@ function addElement(){
 		bDel.value = "Delete";
 		bDel.onclick = function() {
 				itemList.removeChild(document.getElementById("L" + i));
-				names.pop(names[i]);
+				if(names.length == 1){
+					names = [];
+				}
+				else{
+					names = arrayRemove(names, names[i]);
+				}
+				if(val.length == 1){
+					val = [];
+				}
+				else{
+					val = arrayRemove(val, val[i]);
+				}
 		};
 		node.appendChild(textn);
 		node.appendChild(bDel);
 		itemList.appendChild(node);
 	}
+	
+	const pastNamesNode = document.getElementById("pastName");
+	while (pastNamesNode.firstChild) {
+		pastNamesNode.removeChild(pastNamesNode.firstChild);
+	}
+	
+	for(let i = 0; i < pastNames.length; i ++){
+		var node = document.createElement("LI");
+		node.id = "N" + i;
+		var bName = document.createElement("INPUT");
+		bName.type = "button";
+		bName.value = pastNames[i];
+		bName.onclick = function() {
+				document.getElementById("inputName").value = pastNames[i];
+		};
+		node.appendChild(bName);
+		document.getElementById("pastName").appendChild(node);
+	}
+	
 }
 
-function removeElement(input){
-	console.log(input);
-	//document.getElementById("itemList").removeChild(input);
+function sortArrays(){
+	var list = [];
+	for (var j = 0; j < names.length; j++) 
+		list.push({'name': names[j], 'age': val[j]});
+
+	list.sort(function(a, b) {
+		return ((Number(a.age) > Number(b.age)) ? -1 : ((Number(a.age) == Number(b.age)) ? 0 : 1));
+	});
+
+	for (var k = 0; k < list.length; k++) {
+		names[k] = list[k].name;
+		val[k] = list[k].age;
+	}
+}
+
+function arrayRemove(arr, value) {
+
+   return arr.filter(function(ele){
+       return ele != value;
+   });
+
+}
+
+function softReset(){
+	names = [];
+	val = [];
+	
+	const itemListNode = document.getElementById("itemList");
+	while (itemListNode.firstChild) {
+		itemListNode.removeChild(itemListNode.firstChild);
+	}
+}
+
+function hardReset(){
+	names = [];
+	val = [];
+	pastNames = [];
+	
+	const itemListNode = document.getElementById("itemList");
+	while (itemListNode.firstChild) {
+		itemListNode.removeChild(itemListNode.firstChild);
+	}
+	
+	const pastNamesNode = document.getElementById("pastName");
+	while (pastNamesNode.firstChild) {
+		pastNamesNode.removeChild(pastNamesNode.firstChild);
+	}
 }
