a5ce847157eb143fda4d50ab95312b31f744692a
Merthew <timjpatt@gmail.com>
2020-01-16 12:03:55 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function onLoad(){
 	var raw = "";
-	$.get( "http:Merthew.surge.sh/docs/posts.txt", function( data ) {
+	$.get( "http://merthew.surge.sh/docs/posts.txt", function( data ) {
 		var text = data;
 		raw = data;
 		console.log(text);
