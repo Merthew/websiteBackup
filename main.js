991166c279b8f095bd1dd25ab696241bcd637724
Merthew <timjpatt@gmail.com>
2020-01-16 11:49:05 -0500

    update

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function onLoad(){
 	var reader = new FileReader();
+	var posts = new File([""], ".\docs\posts.txt");
+	reader.readAsText(posts);
+	reader.onload = function() {
+		console.log(reader.result);
+	};
+	//console.log(reader.result);
 }
