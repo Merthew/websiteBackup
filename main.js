8468811d4c2539c1d438218cd14a2d86110b0bb2
Merthew <timjpatt@gmail.com>
2020-01-20 16:17:27 -0500

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function onLoad(){
-	$.get( "http://merthew.surge.sh/docs/posts.txt", function( data ) {
+	$.get( "https://merthew.surge.sh/docs/posts.txt", function( data ) {
 		var text = data;
 		console.log(text);
 		document.getElementById("updates").innerHTML = text;
