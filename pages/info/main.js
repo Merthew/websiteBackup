93485ee181a710c609fcd2b3c39c4c1826fbfa85
Merthew <timjpatt@gmail.com>
2020-01-27 17:56:26 -0500

    update

---
+++ pages/info/main.js
@@ -0,0 +1,7 @@
+function onLoad(){
+	$.get( "https://merthew.surge.sh/docs/info.txt", function( data ) {
+		var text = data;
+		console.log(text);
+		document.getElementById("updates").innerHTML = text;
+	});
+}
